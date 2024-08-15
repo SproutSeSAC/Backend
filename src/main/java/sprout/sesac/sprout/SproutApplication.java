8a293f28a089d2a297e48e965879fe3e2bbb12d3
JorimJoram <wkdgyfla97@gmail.com>
2024-08-16 03:58:40 +0900

    for WAR file

--- src/main/java/sprout/sesac/sprout/SproutApplication.java
+++ src/main/java/sprout/sesac/sprout/SproutApplication.java
@@ -2,12 +2,11 @@ package sprout.sesac.sprout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SproutApplication {
-
+public class SproutApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(SproutApplication.class, args);
 	}
-
 }
