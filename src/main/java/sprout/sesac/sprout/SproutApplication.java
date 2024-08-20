0b0267873bc0bf58d60a285f4671950b53cce435
coen park <81370558+Coen90@users.noreply.github.com>
2024-08-20 09:49:53 +0900

    Update SproutApplication.java
    test commit

--- src/main/java/sprout/sesac/sprout/SproutApplication.java
+++ src/main/java/sprout/sesac/sprout/SproutApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SproutApplication{
+public class SproutApplication{//test
 	public static void main(String[] args) {
 		SpringApplication.run(SproutApplication.class, args);
 	}
